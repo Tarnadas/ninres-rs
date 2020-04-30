76ddb62fbdfd7b0e07efe3327ef1de2967e059cd
Mario Reder <mreder1289@gmail.com>
2020-04-30 08:06:25 +0200

    data extraction

--- src/error.rs
+++ src/error.rs
@@ -1,15 +1,23 @@
 use super::*;
 
 use num_enum::TryFromPrimitiveError;
+use std::string::FromUtf8Error;
 
 #[derive(Debug)]
 pub enum SarcError {
     ByteOrderInvalid,
     MagicInvalid,
+    Utf8,
 }
 
 impl From<TryFromPrimitiveError<ByteOrder>> for SarcError {
     fn from(_: TryFromPrimitiveError<ByteOrder>) -> Self {
         Self::ByteOrderInvalid
+    }
+}
+
+impl From<FromUtf8Error> for SarcError {
+    fn from(_: FromUtf8Error) -> Self {
+        Self::Utf8
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -6,15 +6,15 @@ mod error;
 
 use error::*;
 use num_enum::TryFromPrimitive;
-use std::convert::TryFrom;
+use std::{convert::TryFrom, str};
 
 pub type Error = SarcError;
 
 #[derive(Debug)]
-pub struct Sarc {
+pub struct Sarc<'a> {
     header: SarcHeader,
     sfat_header: SfatHeader,
-    sfat_nodes: Vec<SfatNode>,
+    sfat_nodes: Vec<SfatNode<'a>>,
 }
 
 #[derive(Debug)]
@@ -31,8 +31,24 @@ pub struct SfatHeader {
 }
 
 #[derive(Debug)]
-pub struct SfatNode {
+pub struct SfatNode<'a> {
+    hash: u32,
+    attributes: u32,
+    name_table_offset: Option<u32>,
+    name: Option<String>,
+    data_start_offset: u32,
+    data_end_offset: u32,
+    data: &'a [u8],
+}
 
+impl<'a> SfatNode<'a> {
+    fn get_hash(data: &[u32], length: usize, key: u32) -> u32 {
+        let mut result = 0;
+        for i in 0..length {
+            result = data[i] + result * key;
+        }
+        return result;
+    }
 }
 
 #[derive(Clone, Copy, Debug, TryFromPrimitive)]
@@ -43,11 +59,46 @@ pub enum ByteOrder {
 }
 
 pub fn read_sarc(sarc_file: &[u8]) -> Result<Sarc, Error> {
-    let byte_order = ByteOrder::try_from(read_u16(sarc_file, 0x6, ByteOrder::LittleEndian))?;
+    let byte_order = ByteOrder::try_from(read_u16(sarc_file, 0x6, ByteOrder::BigEndian))?;
     let file_size = read_u32(sarc_file, 0x8, byte_order);
     let data_offset = read_u32(sarc_file, 0xC, byte_order);
     let version_number = read_u16(sarc_file, 0x10, byte_order);
     let node_count = read_u16(sarc_file, 0x14 + 0x6, byte_order);
+    let mut sfat_nodes = vec![];
+    let file_name_table_offset = (0x14 + 0xC + node_count as u32 * 0x10) as usize;
+    for i in 0..node_count {
+        let offset = (0x14 + 0xC + i * 0x10) as usize;
+        let hash = read_u32(sarc_file, offset, byte_order);
+        let attributes = read_u32(sarc_file, offset + 0x4, byte_order);
+        let name_table_offset = if attributes & 0xffff0000 == 0x01000000 {
+            Some((attributes & 0x0000ffff) * 4)
+        } else {
+            None
+        };
+        let name = if let Some(name_table_offset) = name_table_offset {
+            let name = sarc_file[(file_name_table_offset + name_table_offset as usize + 8)..]
+                .iter()
+                .take_while(|&n| n != &0u8)
+                .cloned()
+                .collect::<_>();
+            Some(String::from_utf8(name)?)
+        } else {
+            None
+        };
+        let data_start_offset = read_u32(sarc_file, offset + 0x8, byte_order);
+        let data_end_offset = read_u32(sarc_file, offset + 0xC, byte_order);
+        let data = &sarc_file
+            [(data_offset + data_start_offset) as usize..(data_offset + data_end_offset) as usize];
+        sfat_nodes.push(SfatNode {
+            hash,
+            attributes,
+            name_table_offset,
+            name,
+            data_start_offset,
+            data_end_offset,
+            data,
+        })
+    }
     Ok(Sarc {
         header: SarcHeader {
             byte_order,
@@ -55,10 +106,8 @@ pub fn read_sarc(sarc_file: &[u8]) -> Result<Sarc, Error> {
             data_offset,
             version_number,
         },
-        sfat_header: SfatHeader {
-            node_count,
-        },
-        sfat_nodes: vec![],
+        sfat_header: SfatHeader { node_count },
+        sfat_nodes,
     })
 }
 
@@ -67,10 +116,7 @@ fn read_u16(file: &[u8], offset: usize, byte_order: ByteOrder) -> u16 {
         ByteOrder::BigEndian => u16::from_be_bytes,
         ByteOrder::LittleEndian => u16::from_le_bytes,
     };
-    from_bytes([
-        file[offset],
-        file[offset + 1],
-    ])
+    from_bytes([file[offset], file[offset + 1]])
 }
 
 fn read_u32(file: &[u8], offset: usize, byte_order: ByteOrder) -> u32 {
@@ -100,6 +146,7 @@ mod tests {
     #[test_case(MW_MODEL_PACK; "with MW Model Pack")]
     fn test_read_sarc(sarc_file: &[u8]) {
         let sarc_file = read_sarc(sarc_file);
-        dbg!(&sarc_file);
+
+        assert!(sarc_file.is_ok());
     }
 }

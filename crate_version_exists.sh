6e0876003d4bf50fe84e55d3ea2ffb1cca59a2b5
Mario Reder <mreder1289@gmail.com>
2020-10-07 08:08:21 +0200

    ci: fix publish

--- crate_version_exists.sh
+++ crate_version_exists.sh
@@ -4,22 +4,18 @@
     args=("$@")
     LIB_VERSION=${args[0]}
 
-    VERSIONS=$(cargo whatfeatures -l ninres)
-    echo VERSIONS=$VERSIONS
+    VERSIONS=$(cargo whatfeatures -l ninres -s)
 
     VERION_EXISTS=0
 
     while IFS=$'\n' read -ra VERSION; do
-    echo VERSION=${VERSION[@]}
         for V in "${VERSION[@]}"; do
-            echo V=$V
-            echo check=$(echo ninres = ${LIB_VERSION}) eq $V
             if [ "$(echo ninres = ${LIB_VERSION})" = "$V" ]; then
                 VERION_EXISTS=1
                 break
             fi
         done
     done <<< "$VERSIONS"
-} #>/dev/null 2>&1
+} >/dev/null 2>&1
 
 echo $VERION_EXISTS

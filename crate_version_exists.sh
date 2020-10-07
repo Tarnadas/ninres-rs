616dfc0c0692d1b2da9fc5e28ffeb1a27f1682f3
Mario Reder <mreder1289@gmail.com>
2020-10-07 07:52:45 +0200

    debug deploy

--- crate_version_exists.sh
+++ crate_version_exists.sh
@@ -5,17 +5,21 @@
     LIB_VERSION=${args[0]}
 
     VERSIONS=$(cargo whatfeatures -l ninres)
+    echo VERSIONS=$VERSIONS
 
     VERION_EXISTS=0
 
     while IFS=$'\n' read -ra VERSION; do
+    echo VERSION=${VERSION[@]}
         for V in "${VERSION[@]}"; do
+            echo V=$V
+            echo check=$(echo ninres = ${LIB_VERSION}) eq $V
             if [ "$(echo ninres = ${LIB_VERSION})" = "$V" ]; then
                 VERION_EXISTS=1
                 break
             fi
         done
     done <<< "$VERSIONS"
-} >/dev/null 2>&1
+} #>/dev/null 2>&1
 
 echo $VERION_EXISTS
